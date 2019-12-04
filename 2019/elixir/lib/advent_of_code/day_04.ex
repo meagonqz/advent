2a1603f610899b0de8c141559e7854dd6884a67c
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2019-12-04 01:26:54 -0500

    day 04

--- 2019/elixir/lib/advent_of_code/day_04.ex
+++ 2019/elixir/lib/advent_of_code/day_04.ex
@@ -1,7 +1,78 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  def part1() do
+    256_310..732_736
+    |> Enum.filter(&has_atleast_one_double_p1?(&1))
+    |> Enum.filter(&has_consecutive_digits?(&1))
+    |> Enum.count()
   end
 
-  def part2(args) do
+  def has_atleast_one_double_p1?(number) do
+    doubles =
+      number
+      |> Integer.digits()
+      |> Enum.reduce(%{last: 0, list: []}, &is_a_double_p1?(&1, &2))
+
+    length(doubles.list) > 0
+  end
+
+  def is_a_double_p1?(x, %{last: y, list: acc}) do
+    if y == x do
+      %{last: x, list: [x | acc]}
+    else
+      %{last: x, list: acc}
+    end
+  end
+
+  def has_atleast_one_double?(number) do
+    digits = number |> Integer.digits()
+    consec = Enum.reduce(digits, %{last: 0, list: [], prev: false}, &is_a_double?(&1, &2))
+    length(consec.list) > 0
+  end
+
+  def is_a_double?(x, %{last: y, list: acc, prev: prev}) do
+    if y == x do
+      if prev do
+        %{last: x, list: remove_if_exists(x, acc), prev: true}
+      else
+        %{last: x, list: [x | acc], prev: true}
+      end
+    else
+      %{last: x, list: acc, prev: false}
+    end
+  end
+
+  def remove_if_exists(x, acc) do
+    if Enum.member?(acc, x) do
+      List.delete_at(acc, 0)
+    else
+      acc
+    end
+  end
+
+  def has_consecutive_digits?(number) do
+    consec =
+      number
+      |> Integer.digits()
+      |> Enum.reduce(
+        %{last: Enum.at(Integer.digits(number), 0), list: []},
+        &is_less_than_previous?(&1, &2)
+      )
+
+    length(Integer.digits(number)) == length(consec.list)
+  end
+
+  def is_less_than_previous?(x, %{last: y, list: acc}) do
+    if y <= x do
+      %{last: x, list: [x | acc]}
+    else
+      %{last: x, list: acc}
+    end
+  end
+
+  def part2() do
+    256_310..732_736
+    |> Enum.filter(&has_atleast_one_double?(&1))
+    |> Enum.filter(&has_consecutive_digits?(&1))
+    |> Enum.count()
   end
 end
--- 2019/elixir/lib/mix/tasks/d04.p2.ex
+++ 2019/elixir/lib/mix/tasks/d04.p2.ex
@@ -5,13 +5,10 @@ defmodule Mix.Tasks.D04.P2 do
 
   @shortdoc "Day 04 Part 2"
   def run(args) do
-    input = nil
-
     if Enum.member?(args, "-b"),
-      do: Benchee.run(%{part_2: fn -> input |> part2() end}),
+      do: Benchee.run(%{part_2: fn -> part2() end}),
       else:
-        input
-        |> part2()
+        part2()
         |> IO.inspect(label: "Part 2 Results")
   end
 end
