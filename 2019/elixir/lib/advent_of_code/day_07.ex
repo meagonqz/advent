c1a05c36224e05eef905f969cb68dff6482a667c
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2019-12-10 23:17:15 -0500

    day 07, p1

--- 2019/elixir/lib/advent_of_code/day_07.ex
+++ 2019/elixir/lib/advent_of_code/day_07.ex
@@ -1,7 +1,51 @@
 defmodule AdventOfCode.Day07 do
-  def part1(args) do
+  def permutations([]), do: [[]]
+
+  def permutations(list),
+    do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
+
+  def part1() do
+    permutations([4, 3, 2, 1, 0])
+    |> run_phases()
   end
 
-  def part2(args) do
+  def run_phases(phases) do
+    phases
+    |> Enum.map(&scan_each_phase(&1))
+    |> Enum.map(&Enum.to_list/1)
+    |> Enum.map(&Enum.drop(&1, 4))
+    |> List.flatten()
+    |> Enum.max()
+  end
+
+  def scan_each_phase(sequence) do
+    Stream.scan(sequence, 0, &process_phase(&1, &2))
+  end
+
+  def process_phase(phase, input) do
+    "assets/day07_input.txt"
+    |> AdventOfCode.read_file()
+    |> String.trim()
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> process_sequence(phase, input)
+  end
+
+  def process_sequence(puzzle_input, phase, input) do
+    AdventOfCode.Day05.process_sequence(puzzle_input, [phase, input], true)
+  end
+
+  def test() do
+    input =
+      "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    Stream.scan([4, 3, 2, 1, 0], 0, &process_sequence(input, &1, &2))
+    |> Enum.to_list()
+    |> IO.inspect()
+  end
+
+  def part2() do
   end
 end
